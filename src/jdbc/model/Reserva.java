ad023a702e0d820d237ce27a6475a4eb2f291ea6
acosta604 <paulaacosta604@gmail.com>
2023-02-05 22:55:09 -0500

    Clase reserva modificada

--- src/jdbc/model/Reserva.java
+++ src/jdbc/model/Reserva.java
@@ -4,10 +4,62 @@
  */
 package jdbc.model;
 
+import java.util.Date;
+
+
+
 /**
- *
  * @author Paula Acosta
  */
 public class Reserva {
-    
+
+    private Integer id_Reserva;
+    private Date fechaEntrada;
+    private Date fechaSalida;
+    private Double valor;
+    private String formaPago;
+
+    public Reserva(Date fechaE, Date fechaS, Double valor, String formaP) {
+        super();
+        this.fechaEntrada = fechaE;
+        this.fechaSalida = fechaS;
+        this.valor = valor;
+        this.formaPago = formaP;
+
+    }
+
+    public Reserva(Integer idReserva, Date fechaE, Date fechaS, Double valor, String formaP) {
+        this.id_Reserva = idReserva;
+        this.fechaEntrada = fechaE;
+        this.fechaSalida = fechaS;
+        this.valor = valor;
+        this.formaPago = formaP;
+
+    }
+
+    public Integer getId_Reserva() {
+        return id_Reserva;
+    }
+
+    public void setId_Reserva(Integer id_Reserva) {
+        this.id_Reserva = id_Reserva;
+    }
+
+    public Date getFechaEntrada() {
+        return fechaEntrada;
+    }
+
+    public Date getFechaSalida() {
+        return fechaSalida;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public String getFormaPago() {
+        return formaPago;
+    }
+
+
 }
